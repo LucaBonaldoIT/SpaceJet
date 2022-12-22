f87a7b9b462cb350a8718f96851cb1488b0a0eaf
LucaBonaldoIT <lucabonaldoit@gmail.com>
2022-12-22 21:27:52 +0100

    Something

--- ObjectModel/Star.hpp
+++ ObjectModel/Star.hpp
@@ -2,6 +2,7 @@
 #define STAR_H
 
 #include "Game.hpp"
+#include "Random.hpp"
 #include "Node.hpp"
 
 class Star : public Node
@@ -9,26 +10,39 @@ class Star : public Node
 
     private:
 
+    const int SpawnWidthMin = 0;
+    const int SpawnWidthMax = Game::WindowWidth;
+
+    const int SpawnHeightMin = 0;
+    const int SpawnHeightMax = Game::WindowHeight;
+
+    const int SpawnZMin = 0;
+    const int SpawnZtMax = Game::WindowHeight;
+
     public: 
     
     Star()
     {
         _spriteInfo = SpriteInfo("star");
+        
         _widthRatio = 1.0f;
         _heightRatio = 1.0f;
-        _position.x = rand() % 1500;
-        _position.y = rand() % 1500;
-        _position.z = 1280;
+
+        _position.x = Random::getSingleInt(SpawnWidthMin, SpawnWidthMax);
+        _position.y = Random::getSingleInt(SpawnHeightMin, SpawnHeightMax);
+        _position.z = Random::getSingleInt(SpawnZMin, SpawnZtMax);
 
     }
-        Speed getMaxSpeed()
-        {
-            return 0;
-        }
-Speed getDefaultSpeed()
-        {
-            return 0;
-        }
+    
+    Speed getMaxSpeed()
+    {
+        return 0;
+    }
+    
+    Speed getDefaultSpeed()
+    {
+        return 0;
+    }
 };
 
 #endif
